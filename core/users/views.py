f7c8ba275801d3ff2ab1e1670813f79688f130f6
khot-abhishek <abhishekrkhot@gmail.com>
2022-11-03 22:14:11 +0530

    Build: create a registration form and its  view
    create registration form by inheriting usercreationform & create its
    view and the relative urls_path, register.html file

--- core/users/views.py
+++ core/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.views import View
+from django.contrib import messages
 
-# Create your views here.
+
+class RegistrationView(View):
+    def get(self, request):
+        context = {
+            'form': RegistrationForm(),
+        }
+        return render(request, 'users/registration.html', context)
+    
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.validated_data.get('username')
+            messages.success(request, f"Account for '{username}' Successfully created! ")
+            return redirect('login')
+        else:
+            messages.error(request, "Invalid input!, Please fill the correct info")
+            return redirect('register')
